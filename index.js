9e3a3b7276c97ff7b32a25af84f59265c4e05818
Nitin jangid <nitinjangid7878@gmail.com>
2026-05-22 19:29:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let todoList = [
+  {
+  item: 'buy mikl',
+   dueDate: '5/5/26 '} , 
+  {
+    item : 'go to college',
+     dueDate : '5/5/26'
+    }
+];
+displayItems();
+
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+   let dateElement = document.querySelector('#todo-date');
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({item: todoItem , dueDate: todoDate});
+  inputElement.value = '';
+  dateElement.value = '';
+
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector('.todo-container');
+let newHtml = '';
+  for(let i = 0 ; i < todoList.length; i++){
+    let {item , dueDate} = todoList[i];
+newHtml += `
+<span>${item}</span>
+<span>${dueDate}</span>
+<button class="d-button" onclick= "todoList.splice(${i}, 1); displayItems();">Delete</button>
+`;
+  }
+  containerElement.innerHTML = newHtml;
+}
